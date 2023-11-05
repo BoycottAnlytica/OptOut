54746b28d9871afa35613c7eb60902ff88e56f74
Ibrahim <Ibrahim@ebs.net.pk>
2023-11-05 16:56:24 +0100

    initial commit

---
+++ optout/src/main/java/com/optout/optout/OptoutApplication.java
@@ -0,0 +1,13 @@
+package com.optout.optout;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OptoutApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OptoutApplication.class, args);
+	}
+
+}
---
+++ optout/src/test/java/com/optout/optout/OptoutApplicationTests.java
@@ -0,0 +1,13 @@
+package com.optout.optout;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OptoutApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
