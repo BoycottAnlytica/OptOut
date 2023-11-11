bd860472cf27602534faf568338167fb24f0c66e
Ibrahim <Ibrahim@ebs.net.pk>
2023-11-11 19:25:49 +0100

    added test for ProductController

---
+++ optout/src/test/java/com/optout/optout/product/ProductControllerTest.java
@@ -0,0 +1,50 @@
+package com.optout.optout.product;
+
+import static org.mockito.Mockito.anyLong;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+
+import com.optout.optout.company.Company;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+
+@ContextConfiguration(classes = {ProductController.class})
+@ExtendWith(SpringExtension.class)
+class ProductControllerTest {
+
+    @Autowired
+    private ProductController productController;
+
+    @MockBean
+    private ProductService productService;
+
+    /**
+     * Method under test: {@link ProductController#getProduct(long)}
+     */
+    @Test
+    void testGetProduct() throws Exception {
+
+        Company company2 = new Company();
+        Product product3 = new Product();
+        product3.setCompany(company2);
+        when(productService.getProductById(anyLong())).thenReturn(product3);
+        MockHttpServletRequestBuilder requestBuilder =
+                MockMvcRequestBuilders.get("/api/v1/products/{productId}", 1L);
+        MockMvcBuilders.standaloneSetup(productController)
+                .build()
+                .perform(requestBuilder)
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.content().contentType("application/json"))
+                .andExpect(jsonPath("$.company").isNotEmpty());
+
+    }
+}
+
