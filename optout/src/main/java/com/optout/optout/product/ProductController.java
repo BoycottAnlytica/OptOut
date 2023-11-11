dc719242eab861997fdc91dd43b346238495547b
Ibrahim <Ibrahim@ebs.net.pk>
2023-11-11 19:25:39 +0100

    created ProductController

---
+++ optout/src/main/java/com/optout/optout/product/ProductController.java
@@ -0,0 +1,21 @@
+package com.optout.optout.product;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+import static org.springframework.http.HttpStatus.OK;
+
+@RestController
+@RequestMapping("api/v1/products")
+@RequiredArgsConstructor
+public class ProductController {
+
+    private final ProductService productService;
+
+    @GetMapping("{productId}")
+    @ResponseStatus(OK)
+    public Product getProduct(@PathVariable long productId){
+        return productService.getProductById(productId);
+    }
+
+}
