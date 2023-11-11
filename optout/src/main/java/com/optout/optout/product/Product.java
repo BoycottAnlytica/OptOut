7d4376e37a8ae99a824c48dccce7be886e96f905
Ibrahim <Ibrahim@ebs.net.pk>
2023-11-11 19:25:21 +0100

    updated product model

---
+++ optout/src/main/java/com/optout/optout/product/Product.java
@@ -0,0 +1,38 @@
+package com.optout.optout.product;
+
+import com.fasterxml.jackson.annotation.JsonBackReference;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.optout.optout.company.Company;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Entity
+@NoArgsConstructor
+public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(nullable = false)
+    private long id;
+
+    @ManyToOne
+    @JoinColumn(name = "company_id")
+    @JsonIgnoreProperties(value = "product")
+    private Company company;
+    private String name;
+    @Column(nullable = false)
+    private String barcode;
+
+    public Product(ProductRequest product) {
+        this.name = product.name();
+        this.barcode = product.barcode();
+    }
+
+    public Product setCompany(Company company){
+        this.company = company;
+        return this;
+    }
+
+}
