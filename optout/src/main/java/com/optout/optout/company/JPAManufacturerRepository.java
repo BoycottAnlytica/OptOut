f165dc30d1f339483177a244745c6afb7d62a0b8
Ibrahim <Ibrahim@ebs.net.pk>
2023-12-26 23:17:08 +0100

    added findbyName method

--- optout/src/main/java/com/optout/optout/company/JPAManufacturerRepository.java
+++ optout/src/main/java/com/optout/optout/company/JPAManufacturerRepository.java
@@ -3,4 +3,6 @@ package com.optout.optout.company;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface JPAManufacturerRepository extends JpaRepository<Manufacturer, String> {
+    Manufacturer findByName(String s);
+
 }
