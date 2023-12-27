eb51ff467ea8868e386bedb74ba2a4a03b50bebd
Ibrahim <Ibrahim@ebs.net.pk>
2023-12-27 12:13:27 +0100

    optimized imports

--- optout/src/main/java/com/optout/optout/product/JPAProductRepository.java
+++ optout/src/main/java/com/optout/optout/product/JPAProductRepository.java
@@ -2,6 +2,8 @@ package com.optout.optout.product;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface JPAProductRepository extends JpaRepository<Product, String> {
     Optional<Product> findByBarcode(String productBarcode);
 }
