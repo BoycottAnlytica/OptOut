6ff972ae4731ae6eb5d3052029e9c6fb529516eb
Ibrahim <Ibrahim@ebs.net.pk>
2023-12-27 12:13:05 +0100

    optimized imports

--- optout/src/main/java/com/optout/optout/company/JPAManufacturerRepository.java
+++ optout/src/main/java/com/optout/optout/company/JPAManufacturerRepository.java
@@ -2,6 +2,8 @@ package com.optout.optout.company;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface JPAManufacturerRepository extends JpaRepository<Manufacturer, String> {
     Optional<Manufacturer> findByName(String string);
 
