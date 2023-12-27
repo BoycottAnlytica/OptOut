9e2ac5a269d16ef4a82681c7a6ca2ccf4eedc924
Ibrahim <Ibrahim@ebs.net.pk>
2023-12-27 13:50:58 +0100

    added builder for model

--- optout/src/main/java/com/optout/optout/product/Product.java
+++ optout/src/main/java/com/optout/optout/product/Product.java
@@ -6,11 +6,14 @@ import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import lombok.Builder;
+
 
 @Getter
 @Setter
 @Entity
 @NoArgsConstructor
+@Builder
 public class Product {
 
     @Id
